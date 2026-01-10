60379eb5a5816c9075d3e7e3d867254f05822bfe
Kamal <kamalakannan@bng.eco>
2026-01-10 11:33:23 +0530

    Add loading of stored high score

--- src/index.js
+++ src/index.js
@@ -59,6 +59,10 @@ function init() {
     .map(() => Array(COLS).fill(0));
 
   // Set initial target pattern
+
+  // Load high score from localStorage
+highScore = parseInt(localStorage.getItem("stackOverflownHighScore")) || 0;
+document.getElementById("high-score").textContent = highScore;
   setNewTargetPattern();
 
   // Spawn first piece
